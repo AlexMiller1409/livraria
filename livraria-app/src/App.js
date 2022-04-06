5dd5711ea6e02f01f43590c8c611a43a89dfa2dd
alex <alexmiller6796@gmail.com>
2022-04-06 15:22:47 -0300

    add comment on app.js

--- livraria-app/src/App.js
+++ livraria-app/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ListaDeLivros from './Componentes/ListaDeLivros';
 import './App.css';
 
+// app principal
 export default function App() {
   return (
     <ListaDeLivros />
